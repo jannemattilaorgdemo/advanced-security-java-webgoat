48fa3f5c3a5b3d3b4c1106699a3331a35fb8a993
Janne Mattila <janne.mattila@hotmail.fi>
2022-03-01 18:40:34 +0200

    Update start-webwolf.sh

--- webwolf/start-webwolf.sh
+++ webwolf/start-webwolf.sh
@@ -2,6 +2,7 @@
 
 # Script to start WebWolf, it needs a valid database connection from WebGoat so we wait 8 seconds before starting
 # WebWolf application
+token="janne_token_23456"
 
 echo " Waiting for database to be available..."
 sleep 8 && java -Djava.security.egd=file:/dev/./urandom -jar /home/webwolf/webwolf.jar $@
